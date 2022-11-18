00c6ed61a6244ff57c4cde1bdef836c10e58ced0
lavensoft <nhatsdevil@gmail.com>
2022-11-18 18:27:10 +0700

    Upload Image

--- src/duan1/DuAn1.java
+++ src/duan1/DuAn1.java
@@ -3,11 +3,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package duan1;
+import java.io.File;
 import java.util.ArrayList;
+
+import org.bson.Document;
 
 import duan1.config.*;
 import duan1.models.*;
 import duan1.models.product.ProductModel;
+import duan1.utils.HttpClient;
 import duan1.utils.Log;
 import duan1.views.Main;
 import duan1.controllers.*;
@@ -15,7 +19,9 @@ import duan1.controllers.*;
 public class DuAn1 {
     public static void main(String[] args) {
         try {
-            
+            File file = new File("/Users/nhatsdevil/Pictures/LOC22945.png");
+
+            HttpClient.uploadFile(file);
         }catch(Exception e) {
             Log.error(e);
         }

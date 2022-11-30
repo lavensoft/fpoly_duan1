41057a0b4cd31445e28a661e485528957bdb50b6
lavensoft <nhatsdevil@gmail.com>
2022-11-30 19:44:59 +0700

    Update context menu

--- src/duan1/components/ContextMenu.java
+++ src/duan1/components/ContextMenu.java
@@ -1,13 +1,29 @@
 package duan1.components;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.function.Function;
+
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
 
 public class ContextMenu extends JPopupMenu {
     JMenuItem menuItem;
 
-    public ContextMenu() {
-        menuItem = new JMenuItem("Click Me!");
-        add(menuItem);
+    public ContextMenu(ArrayList<Map<String, String>> items, Map<String, Function<Integer, Void>> events) {
+        items.forEach(item -> {
+            JMenuItem menuItem = new JMenuItem(item.get("title"));
+            
+            menuItem.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    events.get(item.get("key")).apply(0);
+                }
+            });
+            
+            add(menuItem);
+        });
     }
 }

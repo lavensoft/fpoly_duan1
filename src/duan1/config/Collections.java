32ba2f0228176c2490a2b8a0e20276e16d47936d
lavensoft <nhatsdevil@gmail.com>
2022-12-06 19:32:07 +0700

    Add promotion

--- src/duan1/config/Collections.java
+++ src/duan1/config/Collections.java
@@ -2,12 +2,12 @@ package duan1.config;
 
 public class Collections {
     public static String USER = "users";
+    public static String PROMOTION = "promotions";
     public static String DEVICE_CONFIGURATION = "device_configurations";
     public static String DIMENSION_CONFIGURATION = "dimension_configurations";
     public static String PRODUCT_MANUFACTURER = "product_manufacturers";
     public static String PRODUCT = "products";
     public static String PRODUCT_DIMENSION = "product_dimensions";
-    public static String PRODUCT_SALE = "product_sales";
     public static String ORDER = "orders";
     public static String ORDER_DIMENSION = "order_dimensions";
     public static String CUSTOMER = "customers";
